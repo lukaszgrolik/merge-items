48af4d1383a77b732bc3ed643b085767723c1cf7
Łukasz Grolik <lukasz@grolik.pl>
2017-02-15 14:58:43 +0100

    Refactor

--- index.js
+++ index.js
@@ -1,102 +1,14 @@
-import _find from 'lodash/find';
-import _map from 'lodash/map';
-import _extend from 'lodash/extend';
-import _each from 'lodash/each';
+import {
+  validateSource,
+  validateOptions,
+  validateItemArg,
+} from './lib/validation';
+import upsertItems from './lib/upsert-items';
 
-const _ = {
-  find: _find,
-  map: _map,
-  extend: _extend,
-  each: _each,
-};
+export default function mergeItems(source, itemArg, opts = {}) {
+  validateSource(source);
+  validateOptions(opts);
+  validateItemArg(itemArg, opts);
 
-function isObject(arg) {
-  return arg instanceof Object && arg instanceof Array === false;
-}
-
-export default function mergeItems(source, itemArg, opts) {
-  if (opts === undefined) opts = {};
-
-  // [step] validate source
-
-  if (source instanceof Array === false) {
-    throw new Error('source must be an array, ' + source + ' given');
-  }
-
-  if (itemArg instanceof Object === false) {
-    throw new Error('itemArg must be either object or array, ' + itemArg + ' given');
-  }
-
-  // [step] validate options
-
-  if (!isObject(opts)) {
-    throw new Error('options must be an object, ' + opts + ' given');
-  }
-
-  if (opts.primaryKey === undefined) opts.primaryKey = 'id';
-
-  if (opts.hasOwnProperty('mapper') && typeof opts.mapper !== 'function') {
-    throw new Error('mapper must be a function, ' + opts.mapper + ' given');
-  }
-
-  if (typeof opts.primaryKey !== 'string' || opts.primaryKey.length === 0) {
-    throw new Error('primaryKey must be a string, ' + opts.primaryKey + ' given');
-  }
-
-  // [step] validate itemArg
-
-  if (isObject(itemArg) && itemArg.hasOwnProperty(opts.primaryKey) === false) {
-    throw new Error('primary key "' + opts.primaryKey + '" is missing in itemArg ' + JSON.stringify(itemArg));
-  }
-
-  if (itemArg instanceof Array) {
-    _.each(itemArg, item => {
-      if (!isObject(item)) {
-        throw new Error('itemArg contains non-object: ' + item);
-      }
-
-      if (item.hasOwnProperty(opts.primaryKey) === false) {
-        throw new Error('primary key "' + opts.primaryKey + '" is missing in object ' + JSON.stringify(item));
-      }
-
-      // look for primaryKey duplicates
-      _.each(itemArg, item2 => {
-        if (itemArg.indexOf(item) !== itemArg.indexOf(item2) &&
-            item[opts.primaryKey] === item2[opts.primaryKey]) {
-          throw new Error('primary key "' + opts.primaryKey + '" in object ' + JSON.stringify(item2) + ' is not unique');
-        }
-      });
-    });
-  }
-
-  // [step] upsert items
-
-  const items = (itemArg instanceof Array) ? itemArg : [itemArg];
-  const insertedItems = [];
-  const updatedItemsIds = [];
-  let record;
-
-  _.each(items, item => {
-    record = _.find(source, record => record[opts.primaryKey] == item[opts.primaryKey]);
-
-    if (record) {
-      // update existing items
-      _.extend(record, item);
-
-      updatedItemsIds.push(record.id)
-    } else {
-      // collect new items
-      insertedItems.push(item);
-    }
-  });
-
-  const newInstances = opts.mapper ? _.map(insertedItems, opts.mapper) : insertedItems;
-
-  // insert new items
-  [].push.apply(source, newInstances);
-
-  return {
-    inserted: _.map(insertedItems, item => item.id),
-    updated: updatedItemsIds,
-  };
+  return upsertItems(source, itemArg, opts);
 };
